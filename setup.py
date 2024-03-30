5a99bcaf9b57b245af79d90565d0224c663e681c
Dennis <dennis@de-jong.frl>
2024-03-30 16:19:09 +0100

    version bump

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     url="https://github.com/dennis1804/homewizard-climate-websocket",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.26",
+    version="0.0.27",
     zip_safe=False,
 )
